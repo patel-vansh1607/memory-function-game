d8ecc1527f11fcf9159d3fa3bd02a878baa8326d
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-23 19:05:22 +0300

    adding the images

--- src/components/game.js
+++ src/components/game.js
@@ -1,27 +1,35 @@
 import "../styles/game.css"
 import { useState } from 'react'
 import Card from './card'
+import htmlImg from '../images/html.png';
+import cssImg from '../images/css.png';
+import jsImg from '../images/js.png';
+import scssImg from '../images/scss.png';
+import reactImg from '../images/react.png';
+import vueImg from '../images/vue.png';
+import angularImg from '../images/angular.png';
+import nodejsImg from '../images/nodejs.png';
 
 
 const MemoryGame = () => {
 
     const [items, setItems] = useState([
-        { id: 1, img: '../images/html.png', stat: "" },
-        { id: 1, img: '../images/html.png', stat: "" },
-        { id: 2, img: '../images/css.png', stat: "" },
-        { id: 2, img: '../images/css.png', stat: "" },
-        { id: 3, img: '../images/js.png', stat: "" },
-        { id: 3, img: '../images/js.png', stat: "" },
-        { id: 4, img: '../images/scss.png', stat: "" },
-        { id: 4, img: '../images/scss.png', stat: "" },
-        { id: 5, img: '../images/react.png', stat: "" },
-        { id: 5, img: '../images/react.png', stat: "" },
-        { id: 6, img: '../images/vue.png', stat: "" },
-        { id: 6, img: '../images/vue.png', stat: "" },
-        { id: 7, img: '../images/angular.png', stat: "" },
-        { id: 7, img: '../images/angular.png', stat: "" },
-        { id: 8, img: '../images/nodejs.png', stat: "" },
-        { id: 8, img: '../images/nodejs.png', stat: "" }
+        { id: 1, img: htmlImg, stat: "" },
+        { id: 1, img: htmlImg, stat: "" },
+        { id: 2, img: cssImg, stat: "" },
+        { id: 2, img: cssImg, stat: "" },
+        { id: 3, img: jsImg, stat: "" },
+        { id: 3, img: jsImg, stat: "" },
+        { id: 4, img: scssImg, stat: "" },
+        { id: 4, img: scssImg, stat: "" },
+        { id: 5, img: reactImg, stat: "" },
+        { id: 5, img: reactImg, stat: "" },
+        { id: 6, img: vueImg, stat: "" },
+        { id: 6, img: vueImg, stat: "" },
+        { id: 7, img: angularImg, stat: "" },
+        { id: 7, img: angularImg, stat: "" },
+        { id: 8, img: nodejsImg, stat: "" },
+        { id: 8, img: nodejsImg, stat: "" }
     ].sort(() => Math.random() - 0.5))
 
 
