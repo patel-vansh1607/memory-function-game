81468bab1f15f580ca67002d6a40ce0e20b8c586
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-23 18:54:32 +0300

    fucntion chec current

--- src/components/game.js
+++ src/components/game.js
@@ -4,6 +4,26 @@ import "../styles/game.css"
 const MemoryGame = () => {
 
     const [prev, setPrev] = useState(-1) 
+
+    function check(current){
+        if(items[current].id == items[prev].id){
+            items[current].stat = "correct"
+            items[prev].stat = "correct"
+            setItems([...items])
+            setPrev(-1)
+        }else{
+            items[current].stat = "wrong"
+            items[prev].stat = "wrong"
+            setItems([...items])
+            setTimeout(() => {
+                items[current].stat = ""
+                items[prev].stat = ""
+                setItems([...items])
+                setPrev(-1)
+            }, 1000)
+        }
+    }
+    
     return(
         <div>
             <div className="game">
